ccf76f6e7d2bd7f86833a5c69167fdc12688847d
SanghyunLee <lgw9736@naver.com>
2025-01-19 19:05:52 +0900

    feat: prettier 적용

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,35 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <header>
-    <h1>
-      ToDo List
-    </h1>
+    <h1>ToDo List</h1>
   </header>
 
   <div class="todo_list">
     <ul>
       <li>
         <div class="todo_item">
-          <input type="checkbox">
           <span>운동하기</span>
+          <div class="todo_item_setting">
+            <button>편집</button>
+            <button>완료</button>
+            <button>삭제</button>
+          </div>
         </div>
       </li>
     </ul>
   </div>
-
 </template>
 
 <style scoped>
-header{
+header {
   width: 100%;
   display: flex;
   justify-content: center;
   padding: 2rem 0;
 }
 
-.todo_list ul{
+.todo_list ul {
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -40,15 +39,15 @@ header{
   padding: 1rem 5rem;
 }
 
-.todo_list ul li{
+.todo_list ul li {
   width: 100%;
   display: flex;
-  border : 1px solid var(--color-border);
+  border: 1px solid var(--color-border);
   padding: 0.5rem;
   border-radius: 0.2rem;
 }
 
-.todo_list ul li .todo_item{
+.todo_list ul li .todo_item {
   display: flex;
   gap: 2rem;
   font-size: 1.5rem;
